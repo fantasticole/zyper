709e29d66e4d438503caa01686a0944ba6015a94
Cole Murphy <cole@spell.run>
2018-10-07 19:37:06 -0400

    Begin building out page elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
+
+import List from "./components/List";
+
 import "./styles/app.scss";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      data: null,
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log({ name })
+    console.log({ value })
+  }
+
+  handleGetData = () => {
+    console.log("handleGetData")
+  }
+
   render() {
     return (
       <div className="app">
-        <h1>hello world</h1>
+        <div className="form">
+          <textarea
+            name="usernames"
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleGetData}>Submit</button>
+        </div>
+        <List data={this.state.data} />
       </div>
     );
   }
---
+++ src/components/List.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const List = (props) => (
+  <div className="list">
+    <h1>List</h1>
+  </div>
+);
+
+export default List;
