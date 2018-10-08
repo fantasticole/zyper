a85f4b0c5a90c05d335eac9aacba66d84ab120b0
Cole Murphy <cole@spell.run>
2018-10-07 21:24:51 -0400

    Add missing sample data

---
+++ src/utils/sampleData.js
@@ -0,0 +1,53 @@
+export const sampleIndividualData = {
+  "0": {
+    "output": {
+      "Average Comments": "4565.6", 
+      "Average Engagement (per post as % of followers)": "1.2", 
+      "Average Engagement per post": "175509.9", 
+      "Average Likes": "170944.3", 
+      "Average Video Views": "0.0", 
+      "Bio": "", 
+      "Counted posts": 12, 
+      "Followers": 15002955, 
+      "Following": 0, 
+      "Number of Hashtags": "6.0", 
+      "Total Engagement": "2106119.0", 
+      "username": "oprah"
+    }
+  }, 
+  "1": {
+    "output": {
+      "Average Comments": "11.2", 
+      "Average Engagement (per post as % of followers)": "29.5", 
+      "Average Engagement per post": "735.3", 
+      "Average Likes": "724.1", 
+      "Average Video Views": "0.0", 
+      "Bio": "BSN, RN | gville or something", 
+      "Counted posts": 12, 
+      "Followers": 2491, 
+      "Following": 296, 
+      "Number of Hashtags": "0.0", 
+      "Total Engagement": "8824.0", 
+      "username": "ellen"
+    }
+  }
+}
+
+export const sampleData = {
+  "0": {
+    "output": {
+      "Average Comments": "4562.8",
+      "Average Engagement (per post as % of followers)": "1.2",
+      "Average Engagement per post": "175426.2",
+      "Average Likes": "170863.3",
+      "Average Video Views": "0.0",
+      "Bio": "",
+      "Counted posts": 12,
+      "Followers": 15002740,
+      "Following": 0,
+      "Number of Hashtags": "6.0",
+      "Total Engagement": "2105114.0",
+      "username": "oprah"
+    }
+  }
+}
