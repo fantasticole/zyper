b09d7ac35329cc5de493d1df474ce74b5a0972a2
Cole Murphy <cole@spell.run>
2018-10-07 21:35:19 -0400

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "./App";
+
+import List from "./components/List";
+
+import { sampleIndividualData } from "./utils/sampleData.js";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders the List", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<List data={sampleIndividualData} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
